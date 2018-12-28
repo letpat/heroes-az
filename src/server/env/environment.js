f9bc710666d1dbb7b34cab88224c81f260c03bcc
letpat <30808532+letpat@users.noreply.github.com>
2018-12-28 16:04:15 -0500

    Delete environment.js

--- src/server/env/environment.js
+++
@@ -1,7 +0,0 @@
-// TODO: replace if yours are different
-module.exports = {
-    accountName: 'letpat-cosmos1-db',
-    databaseName: 'admin',
-    key: 'C244A9HNEqV0tRTZcw5bwCbbIIDMYBM0M0Ao2EyjQFLZrfaTiObI0Y2FppXZhPCqgmc0ffIQfcUCtAw2I8VRLg==',
-    port: 10255
-};
