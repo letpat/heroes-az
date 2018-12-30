d56f7b82a4f8c3a05e49e10ddf5928408b16d947
letpat <letpat@gmail.com>
2018-12-30 11:28:28 -0500

    redistribution

---
+++ src/server/env/environment.js
@@ -0,0 +1,7 @@
+// TODO: replace if yours are different
+module.exports = {
+    accountName: 'letpat-cosmos1-db',
+    databaseName: 'admin',
+    key: 'lC51ZPEdSKSVALQFFIBjSgghbKAQVvFNr6MpEb3coQwjZm2wEayiAEeWZbXm13zhndDIwRb2U4qVgNpLEemwkA%3D%3D',
+    port: 10255
+};
--- src/server/index.js
+++ src/server/index.js
@@ -9,10 +9,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(root, 'dist/angular-cosmosdb')));
+app.use(express.static(path.join(root, 'dist')));
 app.use('/api', routes);
 app.get('*', (req, res) => {
-    res.sendFile('dist/angular-cosmosdb/index.html', { root });
+    res.sendFile('dist/index.html', { root });
 });
 
 app.listen(port, () => console.log(`API running on localhost:${port}`));
