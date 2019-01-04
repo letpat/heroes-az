3630c182b88b57494685dbf0c100527d86959492
letpat <letpat@gmail.com>
2019-01-04 16:18:42 -0500

    retirer publish

--- publish/env/environment.js
+++
@@ -1,7 +0,0 @@
-// TODO: replace if yours are different
-module.exports = {
-    accountName: 'letpat-cosmos1-db',
-    databaseName: 'admin',
-    key: 'lC51ZPEdSKSVALQFFIBjSgghbKAQVvFNr6MpEb3coQwjZm2wEayiAEeWZbXm13zhndDIwRb2U4qVgNpLEemwkA%3D%3D',
-    port: 10255
-};
